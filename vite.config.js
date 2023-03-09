ded5ee90f79d3f097a4aa39bead69046b768e0a7
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2023-03-09 17:56:17 +0300

    'cards'

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,12 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     
-    server: {
-        host: 'localhost',
-    },
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
         }),
     ],
 });
